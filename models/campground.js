e2b1085f0c66beee5473c8777f7562cb78a331b7
cloudman <foskeyt@gmail.com>
2016-12-27 02:08:42 +0000

    First Complete Draft with Google API included

--- models/campground.js
+++ models/campground.js
@@ -12,6 +12,10 @@ var campgroundSchema = new mongoose.Schema({
       },
       username: String
    },
+   loc: {
+      lat: String,
+      long: String
+   },
    comments: [
       {
          type: mongoose.Schema.Types.ObjectId,
