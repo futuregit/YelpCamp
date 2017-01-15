cf03f60316ffe1fdd872da23807238038858f33c
cloudman <foskeyt@gmail.com>
2017-01-15 02:41:24 +0000

    Added Floating semi-transparent button on picture page

--- middleware/index.js
+++ middleware/index.js
@@ -53,8 +53,7 @@ middlewareObj.checkCommentOwnership = function(req, res, next) {
     
 middlewareObj.isLoggedIn = function(req, res, next){
     if(req.isAuthenticated()){
-        
-        return next();
+          return next();
     } 
     console.log("Authenticated")
     req.flash("error", "Please login first");
