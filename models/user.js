f6b40f69e84dae627ce801b70ecd9f8d116d874e
cloudman <foskeyt@gmail.com>
2017-09-18 16:46:57 +0000

    Added go back links in some of the views

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose");
 
 var UserSchema = new mongoose.Schema({
-    usernmae: String,
+    username: String,
     password: String
 });
 
